2ef6e213f6e7ec9d87b3be00b10e1975eddf0fbf
DanAvery19 <loayondan@gmail.com>
2016-09-21 16:00:53 +0800

    Add files via upload

---
+++ FindMinMaxJava.java
@@ -0,0 +1,24 @@
+package findminmax.java;
+
+import java.util.Scanner;
+
+public class FindMinMaxJava {
+    
+    public static void main(String[] args){
+        int[] numbers = { 23, 35, 123, 12412, 1345 };
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for ( int i = 0; i < numbers.length; i ++ ){
+            if (min > numbers[i])
+                min = numbers[i];
+            if (numbers[i] > max)
+                max = numbers[i];
+        }
+        System.out.println("Smallest number is "+ min );
+        System.out.println("Largest number is "+ max );
+
+             
+    }
+    
+}
+
